def09add9c00937a483e2fca0822cabeafc70bca
Avi Avigdor <avigdor@10bis.co.il>
2021-07-25 00:09:09 +0300

    files edited

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-server1: ssh-keygen -t rsa
-server1: cat /home/vagrant/.ssh/id_rsa.pub | ssh vagrant@server1 'cat >> /home/vagrant/.ssh/authorized_keys'
+ssh-keygen -t rsa
+cat /home/vagrant/.ssh/id_rsa.pub | ssh vagrant@server1 'cat >> /home/vagrant/.ssh/authorized_keys'
