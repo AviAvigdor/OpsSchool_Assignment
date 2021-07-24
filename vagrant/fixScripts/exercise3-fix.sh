f057bf6d69bd505d34f671e984bb9847ee63f9ed
Avi Avigdor <avigdor@10bis.co.il>
2021-07-25 00:32:17 +0300

    files edited

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -3,4 +3,4 @@
 sudo chown -R vagrant:www-data /var/www/html
 sudo chmod 755 -R /var/www/html
 sudo sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-available/000-default.conf
-sudo service sshd reload
+sudo service apache2 restart
